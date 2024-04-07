6c2ee9909f34bc21a826ea3b914c572e60784f8a
Lyrafaiqahh <lyrafaiqah@gmail.com>
2024-04-07 12:24:33 +0700

    upload terbaru

---
+++ Sorting.java
@@ -0,0 +1,19 @@
+public class Sorting {
+
+    // SEARCHING
+    // Sequential Search Menggunakan Array 
+    public static void sequentialSearch(int[] arr, int key) {
+        for (int i = 0; i< arr.length; i++) {
+            if ( i == key) {
+                System.out.println("Data ditemukan pada indeks ke-" + i);
+            }
+        }
+
+        System.out.println("Data tidak ditemukan");
+    }
+
+    public static void main(String[] args) {
+        int[] daftarNilai = { 10, 5, 20, 15, 80, 45 };
+        sequentialSearch(daftarNilai, 5);
+    }
+}
---
+++ minggu7/MahasiswaMain.java
@@ -0,0 +1,47 @@
+package minggu7;
+
+import java.util.Scanner;
+public class MahasiswaMain {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Scanner s1 = new Scanner(System.in);
+
+        PencarianMhs data = new PencarianMhs();
+        int jumMhs = 5;
+
+        System.out.println("-------------------------------------");
+        System.out.println("Masukkan data mahasiswa secara Urut dari Nim Terkecil");
+        for(int i = 0; i < jumMhs; i++){
+            System.out.println("------------------------");
+            System.out.print("Nim\t: ");
+            int nim = s.nextInt();
+            System.out.print("Nama\t : ");
+            String nama = s1.nextLine();
+            System.out.print("Umur\t : ");
+            int umur = s.nextInt();
+            System.out.print("IPK\t : ");
+            double ipk = s.nextDouble();
+
+            Mahasiswa m = new Mahasiswa(nim, nama, umur, ipk);
+            data.tambah(m);
+        }
+
+        System.out.println("--------------------------------------");
+        System.out.println("Data keseluruhan Mahasiswa : ");
+        data.tampil();
+
+        System.out.println("_____________________________________");
+        System.out.println("______________________________________");
+        System.out.println("Pencarian Data : ");
+        System.out.println("Masukkan Nim Mahasiswa yang dicari: ");
+        System.out.println("NIM : ");
+        int cari = s.nextInt();
+        System.out.println("menggunakan sequential Search");
+        int posisi = data.FindSeqSearch(cari);
+
+        data.Tampilposisi(cari, posisi);
+
+        data.TampilData(cari, posisi);
+        
+    }
+}
